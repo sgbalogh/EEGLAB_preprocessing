65af9d1543f4ac4ee9015415a1dd049715d9057f
Stephen Balogh <sgbalogh@gmail.com>
2015-08-18 23:01:30 -0400

    changes while loop, adds EVENTLIST.nbin checker instead of input

--- parse_apart_bins.m
+++ parse_apart_bins.m
@@ -1,43 +1,41 @@
 subject_list = {'A_NS707.002'};
 eeglab
 EEG = pop_loadset('/Users/stephen/Desktop/neuro/trynow_event.set');
-numbins = 6;
+
+numbins = EEG.EVENTLIST.nbin;
+
 bin = 1;
 bin_epochs = struct();
 eventlist_rows = length(EEG.EVENTLIST.eventinfo);
 disp(eventlist_rows);
+
 while bin <= numbins
 initnum = 1;
 marked_accept = [];
 marked_reject = [];
-    while initnum < eventlist_rows
+    while initnum <= eventlist_rows
 
-    
     if EEG.EVENTLIST.eventinfo(initnum).bini == bin
         epoch = EEG.EVENTLIST.eventinfo(initnum).bepoch;
         if EEG.EVENTLIST.eventinfo(initnum).flag == 0
         marked_accept(end+1) = epoch;
         elseif EEG.EVENTLIST.eventinfo(initnum).flag == 1
         marked_reject(end+1) = epoch;
-        end
-        
+        end   
     end
-
     initnum = initnum + 1;
-
     end
 
 strbin = num2str(bin);
 strbinacc = ['bin' strbin '_accepted'];
 strbinrej = ['bin' strbin '_rejected'];
-
 bin_epochs = setfield(bin_epochs,strbinacc,marked_accept);
 bin_epochs = setfield(bin_epochs,strbinrej,marked_reject);
 disp(bin_epochs.(strbinacc));
 
 bin = bin + 1;
-
 end
+
 bin = 1;
 while bin <= numbins
    
